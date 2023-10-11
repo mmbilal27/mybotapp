69d6e241e1515c7e17d76021fc045be1035f037f
mmbilal27 <mmbilal27@gmail.com>
2023-10-11 22:57:22 +0500

    button added

--- myapp.py
+++ myapp.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title("my practice bot app")
 st.write("Lets create a bot app on streamlit")
-st.button("Your Name")
+btn = st.button("Your Name")
+
+if btn:
+    st.success('You clicked the button!')
+
